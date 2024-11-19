d7b00dff5f8362177d68af0203c93ae44b75f646
Ghanshyam Mali <somecorpdev@outlook.com>
2024-11-19 12:31:45 +0530

    Added basic folder structre for dashboard

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,10 +1,20 @@
 import { Navigate } from "react-router-dom";
 import PageNotFound from "../components/NotFound/PageNotFound";
+import { UserDashboard } from "../pages/dashboard";
+import { UserLandingPage } from "../pages/landingPage";
 
 const childRoutes = [
   {
     path: "*",
     element: <PageNotFound />,
+  },
+  {
+    path: "",
+    element: <UserDashboard />,
+  },
+  {
+    path: "/landingPage",
+    element: <UserLandingPage />,
   },
 ];
 
