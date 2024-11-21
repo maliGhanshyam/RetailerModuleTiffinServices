9ab51a66b46b07ea617ff457a5426f965be33197
Rahul <rahulbuddies007@gmail.com>
2024-11-22 01:06:57 +0530

    protected route implemented

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -2,15 +2,25 @@ import { Navigate } from "react-router-dom";
 import PageNotFound from "../components/NotFound/PageNotFound";
 import { LoginForm } from "../pages/LoginPage";
 import AddTiffinForm from "../pages/AddTiffinPage/AddTiffinForm";
+import ProtectedRoute from "./ProtectedRoute";
+import { RETAILER_ID } from "../constants/ROLES";
 
 const childRoutes = [
   {
     path: "login",
-    element: <LoginForm />,
+    element: (
+      <ProtectedRoute guestOnly={true}>
+        <LoginForm />
+      </ProtectedRoute>
+    ),
   },
   {
     path: "addTiffin",
-    element: <AddTiffinForm />,
+    element: (
+      <ProtectedRoute requiredRole={RETAILER_ID}>
+        <AddTiffinForm />
+      </ProtectedRoute>
+    ),
   },
   {
     path: "*",
